3416471b738ccfa7ee25b3e08e44d9964e5474a9
Neo Phukubye <nephujhb025@student.wethinkcode.co.za>
2026-07-18 10:09:55 +0200

    progress

--- tracebot/main.py
+++ tracebot/main.py
@@ -144,6 +144,4 @@ async def root():
     return FileResponse("../docs/index.html")
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
